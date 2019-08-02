b353f21254fdd1c9cb63671c9b31cdd2c9326345
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-03 00:15:55 +0200

    update #21

--- app/Models/MedicalBoard.php
+++ app/Models/MedicalBoard.php
@@ -18,20 +18,20 @@ use Illuminate\Database\Eloquent\Model;
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\Models\Employ $employ
  * @property-read \App\User $user
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereEmpId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereField($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereRegistrationDate($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereRegistrationNumber($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard query()
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereEmpId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereField($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereRegistrationDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereRegistrationNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereUserId($value)
  * @mixin \Eloquent
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\MedicalBoard whereEmployId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MedicalBoard whereEmployId($value)
  */
 class MedicalBoard extends Model
 {
