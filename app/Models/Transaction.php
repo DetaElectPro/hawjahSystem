70cb7f79755deaaa40ca7a28c60256572a85fde4
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-16 00:12:56 +0200

    update #48

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['type', 'amount'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
