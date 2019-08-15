fd57b696c8e64635d691b9d01fd606f814cfafc7
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-15 17:11:06 +0200

    update #38

--- app/Models/FeaturedCenters.php
+++ app/Models/FeaturedCenters.php
@@ -12,6 +12,6 @@ class FeaturedCenters extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
