b2151802be0dff7ead650199fa793998ca15704c
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-16 00:17:38 +0200

    update #51

--- database/migrations/2019_07_30_072935_create_emergency_services_table.php
+++ database/migrations/2019_07_30_072935_create_emergency_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEmergencyServicedsTable extends Migration
+class CreateEmergencyServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEmergencyServicedsTable extends Migration
      */
     public function up()
     {
-        Schema::create('emergency_serviceds', function (Blueprint $table) {
+        Schema::create('emergency_services', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('address');
