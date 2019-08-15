e9565638a430a91a77feae88b25904072a344ab8
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-15 17:04:34 +0200

    update #35

---
+++ app/Models/FeaturedCenters.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FeaturedCenters extends Model
+{
+    protected $fillable = [];
+    protected $table = 'featured_centers';
+
+
+}
