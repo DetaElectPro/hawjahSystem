49b23940902b42474363abb27381726ab3b562bd
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-16 00:00:12 +0200

    update #45

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
     protected $fillable = ['balance', 'wallet_number'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
