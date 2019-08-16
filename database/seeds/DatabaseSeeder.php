83265a1543952bfec12af590afe4f74ad8f46550
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-16 20:18:08 +0200

    update #60 add unit test for users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
         $this->call(usersTableSeed::class);
         $this->call(employsTableSeed::class);
         $this->call(medicaBoradsTableSeeder::class);
-        $this->call(medicaFieldsTableSeeder::class);
-        $this->call(medicaSpecialtiesTableSeeder::class);
-        $this->call(requestSpecialistsTableSeeder::class);
-        $this->call(acceptRequestsTableSeeder::class);
+//        $this->call(medicaFieldsTableSeeder::class);
+//        $this->call(medicaSpecialtiesTableSeeder::class);
+//        $this->call(requestSpecialistsTableSeeder::class);
+//        $this->call(acceptRequestsTableSeeder::class);
         $this->call(emergencyServicedsTableSeeder::class);
     }
 }
