dadb88f71e6f0b93dd8585ae48fc3209b6f7f97a
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-16 20:51:46 +0200

    update #61

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -17,4 +17,25 @@ class UserTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    /** @test */
+    public function it_can_create_a_carousel()
+    {
+        $data = [
+            'name' => $this->faker->word,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phone,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role_id' => 2,
+            'remember_token' => Str::random(10),
+        ];
+
+        $carouselRepo = new CarouselRepository(new Carousel);
+        $carousel = $carouselRepo->createCarousel($data);
+
+        $this->assertInstanceOf(Carousel::class, $carousel);
+        $this->assertEquals($data['title'], $carousel->title);
+        $this->assertEquals($data['link'], $carousel->link);
+        $this->assertEquals($data['image_src'], $carousel->src);
+    }
 }
