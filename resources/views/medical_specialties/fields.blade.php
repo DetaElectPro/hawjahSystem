a01efd2f699d8f3f6145174b31a85831ef2981af
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-17 06:41:51 +0200

    update #73

--- resources/views/medical_specialties/fields.blade.php
+++ resources/views/medical_specialties/fields.blade.php
@@ -69,13 +69,15 @@
         <button onclick="myFunction()" class="dropbtn">Dropdown</button>
         <div id="myDropdown" class="dropdown-content">
             <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
-            <a href="#about">About</a>
-            <a href="#base">Base</a>
-            <a href="#blog">Blog</a>
-            <a href="#contact">Contact</a>
-            <a href="#custom">Custom</a>
-            <a href="#support">Support</a>
-            <a href="#tools">Tools</a>
+{{--            <a href="#about">About</a>--}}
+{{--            <a href="#base">Base</a>--}}
+{{--            <a href="#blog">Blog</a>--}}
+{{--            <a href="#contact">Contact</a>--}}
+{{--            <a href="#custom">Custom</a>--}}
+{{--            <a href="#support">Support</a>--}}
+            @foreach($field as $item)
+            <a href="#{{$item->name}}">{{$item->name}}</a>
+                @endforeach
         </div>
     </div>
 
