14af5c0ddc7319388322d6c38659ac31ddf7b71d
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-17 06:33:26 +0200

    update #70

--- resources/views/medical_specialties/fields.blade.php
+++ resources/views/medical_specialties/fields.blade.php
@@ -2,6 +2,56 @@
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
+    <div class="form-group col col-md-6 has-feedback {{ $errors->has('name') ? ' has-error' : '' }}">
+        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="name">
+        <span class="glyphicon glyphicon-earname form-control-feedback"></span>
+        @if ($errors->has('name'))
+            <span class="help-block">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+        @endif
+    </div>
+
+
+    <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+        <div id="myDropdown" class="dropdown-content">
+            <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+            <a href="#about">About</a>
+            <a href="#base">Base</a>
+            <a href="#blog">Blog</a>
+            <a href="#contact">Contact</a>
+            <a href="#custom">Custom</a>
+            <a href="#support">Support</a>
+            <a href="#tools">Tools</a>
+        </div>
+    </div>
+
+
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
     <a href="{!! route('medicalSpecialties.index') !!}" class="btn btn-default">Cancel</a>
 </div>
+
+
+
+<script>
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    function filterFunction() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+            txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+            }
+        }
+    }
+</script>
