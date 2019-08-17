f1eaabe325523cb8d4afe720a5ee9050f7a900e5
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-17 06:24:27 +0200

    update #69

--- resources/views/medical_fields/create.blade.php
+++ resources/views/medical_fields/create.blade.php
@@ -13,15 +13,6 @@
             <div class="box-body">
                 <div class="row">
                     {!! Form::open(['route' => 'medicalFields.store']) !!}
-                    <div class="form-group has-feedback {{ $errors->has('name') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="name">
-                        <span class="glyphicon glyphicon-earname form-control-feedback"></span>
-                        @if ($errors->has('name'))
-                            <span class="help-block">
-                    <strong>{{ $errors->first('name') }}</strong>
-                </span>
-                        @endif
-                    </div>
 
                         @include('medical_fields.fields')
 
--- resources/views/medical_fields/fields.blade.php
+++ resources/views/medical_fields/fields.blade.php
@@ -1,4 +1,13 @@
 
+<div class="form-group col col-md-6 has-feedback {{ $errors->has('name') ? ' has-error' : '' }}">
+    <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="name">
+    <span class="glyphicon glyphicon-earname form-control-feedback"></span>
+    @if ($errors->has('name'))
+        <span class="help-block">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+    @endif
+</div>
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
