b3945d36f90aae504617ba7fd4861066c41a010e
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-18 07:57:25 +0200

    update #89

--- app/Models/EmergencyServices.php
+++ app/Models/EmergencyServices.php
@@ -37,6 +37,36 @@ class EmergencyServices extends Model
     protected $table = 'emergency_services';
     protected $fillable = ['name', 'address', 'price_per_day', 'available_bed', 'type', 'user_id'];
 
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'address' => 'string',
+        'price_per_day' => 'float',
+        'available_bed' => 'integer',
+        'type' => 'string',
+        'user_id' => 'integer',
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required|min:3',
+        'address' => 'required|min:3',
+        'price_per_day' => 'required',
+        'available_bed' => 'required|min:1',
+        'type' => 'required|min:3',
+    ];
+
+
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
