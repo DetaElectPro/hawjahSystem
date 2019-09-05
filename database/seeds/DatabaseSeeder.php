0ee189f627f663556d2d7fed263c6e40b134a1ce
Tayeb-Ali <Mr.elteb@gmail.com>
2019-09-05 12:54:33 +0200

    update :) #4

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,16 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(usersTableSeed::class);
+        $this->call(usersTableSeed::class);
         $this->call(UsersSeeder::class);
         $this->call(RolesSeeder::class);
         $this->call(UsersRolesSeeder::class);
-//        $this->call(employsTableSeed::class);
-//        $this->call(medicaBoradsTableSeeder::class);
-//        $this->call(medicaFieldsTableSeeder::class);
-//        $this->call(medicaSpecialtiesTableSeeder::class);
-//        $this->call(requestSpecialistsTableSeeder::class);
-//        $this->call(acceptRequestsTableSeeder::class);
+        $this->call(employsTableSeed::class);
+        $this->call(medicaBoradsTableSeeder::class);
+        $this->call(medicaFieldsTableSeeder::class);
+        $this->call(medicaSpecialtiesTableSeeder::class);
+        $this->call(requestSpecialistsTableSeeder::class);
+        $this->call(acceptRequestsTableSeeder::class);
 //        $this->call(emergencyServicedsTableSeeder::class);
     }
 }
