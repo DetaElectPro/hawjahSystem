c800cd6624eb12b8412be3ac75ab934bdabbc462
Tayeb-Ali <Mr.elteb@gmail.com>
2019-09-10 13:53:29 +0200

    try do som

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(usersTableSeed::class);
+        // $this->call(usersTableSeed::class);
         $this->call(UsersSeeder::class);
         $this->call(RolesSeeder::class);
         $this->call(UsersRolesSeeder::class);
