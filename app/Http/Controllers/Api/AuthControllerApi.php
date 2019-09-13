9134080208ac45c9c983ed496a359c140eee1955
Tayeb-Ali <Mr.elteb@gmail.com>
2019-09-13 18:42:47 +0200

    auth upload image or save default

--- app/Http/Controllers/Api/AuthControllerApi.php
+++ app/Http/Controllers/Api/AuthControllerApi.php
@@ -2,34 +2,41 @@
 
 namespace App\Http\Controllers\Api;
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use JWTAuth;
 use Illuminate\Http\Request;
 use App\Models\Auth\User\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class AuthControllerApi extends Controller
 {
     public function register(Request $request)
     {
         $status = 1;
-          
         $validator = Validator::make($request->all(), [
-           'email' => 'email|unique:users',
-           'name' => 'required|max:255',
+            'email' => 'email|unique:users',
+            'name' => 'required|max:255',
             'phone' => 'required|unique:users|max:10',
             'password' => 'required|max:30|min:6'
-       ]);
-        
-       if ($validator->fails()) {
-            return response()->json(["message"=> $validator->messages()->first(), "error"=> true]);
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["message" => $validator->messages()->first(), "error" => true]);
         }
+        $image = self::saveImage($request);
+
         $user = User::create([
             'phone' => $request->phone,
             'name' => $request->name,
             'role_id' => $request->role_id,
             'status' => $status,
             'password' => $request->password,
+            'image' => $image,
         ]);
 
         $token = auth('api')->login($user);
@@ -71,15 +78,15 @@ class AuthControllerApi extends Controller
         try {
             $token = JWTAuth::parseToken()->authenticate();
             return response()->json(["message" => "valid token", "status" => true]);
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        } catch (TokenExpiredException $e) {
 
             return response()->json(["message" => "token is expired", 'status' => false]);
 
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        } catch (TokenInvalidException $e) {
             return response()->json(["message" => "token is invalid", 'status' => false]);
             // do whatever you want to do if a token is invalid
 
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+        } catch (JWTException $e) {
             return response()->json(["message" => "token is not present", 'status' => false]);
             // do whatever you want to do if a token is not present
         }
@@ -90,15 +97,15 @@ class AuthControllerApi extends Controller
         try {
             $user = JWTAuth::parseToken()->authenticate();
             return $data = User::whereId($user->id)->with('employ', 'medical_board')->get(['id', 'name', 'phone', 'created_at']);
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        } catch (TokenExpiredException $e) {
 
             return response()->json(["message" => "token is expired", 'status' => false]);
 
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        } catch (TokenInvalidException $e) {
             return response()->json(["message" => "token is invalid", 'status' => false]);
             // do whatever you want to do if a token is invalid
 
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+        } catch (JWTException $e) {
             return response()->json(["message" => "token is not present", 'status' => false]);
             // do whatever you want to do if a token is not present
         }
@@ -109,4 +116,25 @@ class AuthControllerApi extends Controller
         return User::whereId($id)->with('employ', 'medical_board')->get(['id', 'name', 'phone', 'created_at']);
     }
 
+    public function saveImage($request)
+    {
+        $random = Str::random(10);
+        if ($request->hasfile('image')) {
+            $image = $request->file('image');
+            $name = $random . 'profile_' . self::dateNow() . ".jpg";
+            $image->move(public_path() . '/upload/image/', $name);
+            $name = url("upload/image/$name");
+            return $name;
+        }
+        return url("upload/image/profile.jpg");
+    }
+
+    /**
+     * @return string
+     */
+    public function dateNow()
+    {
+        $mytime = Carbon::now();
+        return $mytime->toDateTimeString();
+    }
 }
