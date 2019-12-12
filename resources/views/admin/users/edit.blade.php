62c36fed5472a7e01fcc947461063fbaa72ed75f
Tayeb-Ali <Mr.elteb@gmail.com>
2019-12-12 14:09:30 +0200

    release 3 update #13

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -46,7 +46,7 @@
             </div>
             <div class="form-group">
                 <label class="control-label col-md-3 col-sm-3 col-xs-12" for="phone">
-                    Status:
+                    Phone:
                     <span class="required">*</span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
