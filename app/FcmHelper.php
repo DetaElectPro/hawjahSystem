ae1f1f79c4c7ffa71337c95b0a7b0e7c4516e9ed
Tayeb-Ali <Mr.elteb@gmail.com>
2020-01-21 09:06:14 +0200

    update Fcm try Guzzle

--- app/FcmHelper.php
+++ app/FcmHelper.php
@@ -16,7 +16,7 @@ class FcmHelper extends Model
         $registatoin_ids = [$data->fcm_registration_id];
         $message = ['title' => $data->title, 'body' => $data->message];
         //Google cloud messaging GCM-API url
-        $url = 'https://fcm.googleapis.com/fcm/send';
+//        $url = 'https://fcm.googleapis.com/fcm/send';
         $fields = ['registration_ids' => $registatoin_ids, 'data' => ['message' => $message]];
 
         define("GOOGLE_API_KEY", env('FCM_SERVER_KEY'));
@@ -24,19 +24,31 @@ class FcmHelper extends Model
         $headers = [
             'Authorization: key=' . GOOGLE_API_KEY,
             'Content-Type: application/json'];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-        $result = curl_exec($ch);
-        if ($result === FALSE) {
-            die('Curl failed: ' . curl_error($ch));
-        }
-        curl_close($ch);
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_POST, true);
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+//        $result = curl_exec($ch);
+//        if ($result === FALSE) {
+//            die('Curl failed: ' . curl_error($ch));
+//        }
+//        curl_close($ch);
+        $result = $this->fcm_request($fields, $headers);
         return $result;
+    }
+
+    private function fcm_request($fields, $headers)
+    {
+        $client = new \GuzzleHttp\Client($headers);
+        $url = 'https://fcm.googleapis.com/fcm/send';
+
+        $request = $client->post($url, json_encode($fields));
+        $response = $request->send();
+
+        return $response;
     }
 }
