d7bac06995b3547ccd1332a7c605f3d2069ad381
Tayeb-Ali <Mr.elteb@gmail.com>
2020-09-03 10:25:19 +0100

    update web route

--- routes/web.php
+++ routes/web.php
@@ -17,46 +17,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('dashboard', 'Web\DashboardWEBController@index');
-
-//blog
-Route::get('blog', 'Web\BlogWEBController@index');
-Route::post('blog', 'Web\BlogWEBController@store');
-Route::get('blog/create', 'Web\BlogWEBController@create');
-Route::get('blog/{id}/view', 'Web\BlogWEBController@show');
-Route::get('blog/{id}/edit', 'Web\BlogWEBController@edit');
-Route::put('blog/{id}/update', 'Web\BlogWEBController@update');
-Route::get('blog/{id}/delete', 'Web\BlogWEBController@destroy');
-//Users
-Route::get('users', 'Web\UserWEBController@index');
-Route::post('users', 'Web\UserWEBController@store');
-Route::get('users/create', 'Web\UserWEBController@create');
-Route::get('users/{id}/view', 'Web\UserWEBController@show');
-Route::get('users/{id}/edit', 'Web\UserWEBController@edit');
-Route::put('users/{id}/update', 'Web\UserWEBController@update');
-Route::get('users/{id}/delete', 'Web\UserWEBController@destroy');
-//MedicalField
-Route::get('medical_fields', 'Web\MedicalFieldWEBController@index');
-Route::post('medical_fields', 'Web\MedicalFieldWEBController@store');
-Route::get('medical_fields/create', 'Web\MedicalFieldWEBController@create');
-Route::get('medical_fields/{id}/view', 'Web\MedicalFieldWEBController@show');
-Route::get('medical_fields/{id}/edit', 'Web\MedicalFieldWEBController@edit');
-Route::put('medical_fields/{id}/update', 'Web\MedicalFieldWEBController@update');
-Route::get('medical_fields/{id}/delete', 'Web\MedicalFieldWEBController@destroy');
-//medical_specialists
-Route::resource('medical_specialists', 'Web\MedicalSpecialtyWEBController');
-//Route::post('medical_specialists', 'Web\MedicalSpecialtyWEBController@store');
-//Route::get('medical_specialists/create', 'Web\MedicalSpecialtyWEBController@create');
-//Route::get('medical_specialists/{id}/view', 'Web\MedicalSpecialtyWEBController@show');
-//Route::get('medical_specialists/{id}/edit', 'Web\MedicalSpecialtyWEBController@edit');
-//Route::put('medical_specialists/{id}/update', 'Web\MedicalSpecialtyWEBController@update');
-//Route::get('medical_specialists/{id}/delete', 'Web\MedicalSpecialtyWEBController@destroy');
-
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('dashboard', 'Web\DashboardWEBController@index');
+
+Route::group(['prefix' => 'admin'], function () {
+
+//blog
+    Route::resource('blog', 'Web\BlogWEBController');
+
+//Users
+    Route::resource('users', 'Web\UserWEBController');
+
+//MedicalField
+    Route::resource('medical_fields', 'Web\MedicalFieldWEBController');
+
+//medical_specialists
+    Route::resource('medical_specialists', 'Web\MedicalSpecialtyWEBController');
+
+});
 
 Route::group(['prefix' => 'chart'], function () {
     Route::get('user', 'Web\DashboardWEBController@users');
