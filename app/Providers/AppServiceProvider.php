960a7dca03bf7e4365071535f068be9b304391da
Tayeb-Ali <Mr.elteb@gmail.com>
2020-09-03 06:55:09 +0100

    update appServicePro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(IdeHelperServiceProvider::class);
-
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
     }
 
     /**
